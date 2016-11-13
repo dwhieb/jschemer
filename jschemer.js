363d995fcb54f91370f3a8a4bb446ba1ff8b3bce
Alyanna Landayan <alyanna.landayan@gmail.com>
2016-11-13 15:36:22 -0800

    saved changes

--- jschemer.js
+++ jschemer.js
@@ -1,11 +1,13 @@
-function jschemer(path, options) {
-  // TODO: make sure options is an object. if options doesn't exist, set to default. if it does exist, make sure it's an object. if not an object, it will have a typed error.
+function jschemer(path, options = {}) {
+
   if (typeof path !== "string") {
     throw new TypeError ("Make sure the path is a string.");
-  };
+  }
+
   if (typeof options !== "object") {
     throw new TypeError ("Make sure options is an object.");
   }
-}
+
+};
 
 module.exports = jschemer;
