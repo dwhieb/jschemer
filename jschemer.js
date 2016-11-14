ea6867b4fa94a3a6069f2292df51412c54cfcabd
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-13 23:28:35 -0800

    comment code

--- jschemer.js
+++ jschemer.js
@@ -21,14 +21,19 @@ if (require.main) {
   const list = val => val.replace(/ /g, '').split(',');
 
   program
-  .version(meta.version)
-  .arguments('<path>')
+  .version(meta.version) // set version
+  .arguments('<path>')   // set required <path> argument
+
+  // set options
   .option('-c, --css <filename>', `The path to the CSS file to use for styling the documentation. Defaults to 'out/jschemer.css'.`)
   .option('-i, --ignore <filenames>', `A list of filenames to ignore.`, list)
   .option('-o, --out <directory>', `The name of the directory to output files to. Defaults to 'out'.`)
   .option('-r, --readme <filename>', `A readme file (in Markdown) to include in the generated documentation.`)
+
+  // run this once the arguments are parsed
   .action(path => {
 
+    // collect the options passed to the command line
     const options = {
       css:    program.css,
       ignore: program.ignore,
@@ -36,10 +41,18 @@ if (require.main) {
       readme: program.readme,
     };
 
+    // run jschemer using the passed options
     jschemer(path, options);
 
   })
+
+  // parse the command line arguments
   .parse(process.argv);
+
+  // throw an error if the <path> argument is missing
+  if (!program.args.length) {
+    throw new Error(`A <path> argument must be provided. The <path> argument may either be a single file or a directory.`);
+  }
 
 }
 
