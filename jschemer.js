0b0f12fe7657b81c3da219bb8961a092dec47e7b
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-13 21:52:02 -0800

    clean up master branch

--- jschemer.js
+++ jschemer.js
@@ -1,17 +1,12 @@
-function jschemer(path, options = {}) {
+const jschemer = function(path, options = {}) {
 
-  if (typeof path !== "string") {
-    throw new TypeError ("Make sure the path is a string.");
+  if (typeof path !== 'string') {
+    throw new TypeError(`The 'path' argument must be a string.`);
   }
 
-  if (typeof options !== "object") {
-    throw new TypeError ("Make sure options is an object.");
+  if (typeof options !== 'object') {
+    throw new TypeError(`The 'options' argument must be an object.`);
   }
-
-  const file = 'data.json';
-
-  // TODO: read the file using the 'file' variable (above), and save it to the 'schema variable'
-  // test it by logging the 'schema' variable to the console
 
 };
 
