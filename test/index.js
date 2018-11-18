57c5db45660cbaa71182d127cfd532b957291a6a
Daniel W. Hieber <dwhieb@gmail.com>
2018-11-18 01:26:12 -0600

    TEST: jschemer generates a landing page

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
-const jschemer = require(`../src`);
+const jschemer     = require(`../src`);
+const { readFile } = require(`fs`).promises;
 
 describe(`jschemer`, function() {
 
-  it(`exists`, function() {
-    expect(jschemer).toBeDefined();
+  it(`generates a landing page`, async function() {
+    const index = await readFile(`../out/index.html`, `utf8`);
+    expect(index).toBeDefined();
   });
 
 });
